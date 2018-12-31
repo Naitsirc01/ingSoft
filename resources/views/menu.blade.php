a557e956cff6b96966d71b22509fc084dd528385
ignacio <icm011@alumnos.ucn.cl>
2018-12-31 01:42:13 -0300

    menu pormientras

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,18 +1,22 @@
-@extends('layout.mainlayout')
+@extends('layout.formlayout')
 @section('content')
-
+  
   <div>
+    <div class="container">
+      <div class="row main">
+        <div class="main-login main-center">
+    <h1>MENÚ</h1>
 
-    <h1>Titulo</h1>
-
-    <p>que pasa</p>
     <ul>
-      <li><a href="default.asp">Home</a></li>
-      <li><a href="news.asp"></a></li>
-      <li><a href="contact.asp">Contact</a></li>
-      <li><a href="about.asp">About</a></li>
+      <li><a href="/registroCon" style="color:white;">Registro Convenio</a></li>
+      <li><a href="/registroas" style="color:white;">Registro Aprendizaje y Servicios</a></li>
+      <li><a href="/registroExtension" style="color:white;">Registro Extension</a></li>
+      <li><a href="/registroTit" style="color:white;">Registro Titulación</a></li>
+      <li><a href="http://ing.net/" style="color:white;">Cerrar Sesión</a></li>
     </ul>
-
+        </div>
+      </div>
+    </div>
   </div>
 
 @endsection
